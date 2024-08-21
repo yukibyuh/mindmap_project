6e8a908fbd0963b0a8c53d5364231386db36c9f6
yukibyuh <133543600+yukibyuh@users.noreply.github.com>
2024-08-20 20:13:16 -1000

    Second commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './styles.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
